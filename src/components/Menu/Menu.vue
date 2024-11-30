66a2763503b39fa086bdf3673ca3e53f75d52e9a
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-11-30 18:30:25 +0000

    menu updated

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav :class="$style.menu">
+    <router-link
+      to="/first"
+      :class="[$style.link, { [$style.active]: isActive('/first') }]">
+      First
+    </router-link>
+    <router-link
+      to="/second"
+      :class="[$style.link, { [$style.active]: isActive('/second') }]">
+      Second
+    </router-link>
+    <router-link
+      to="/third"
+      :class="[$style.link, { [$style.active]: isActive('/third') }]">
+      Third
+    </router-link>
+  </nav>
+</template>
+<script>
+export default {
+  name: 'Menu',
+  methods: {
+    isActive(route) {
+      return this.$route.path === route;
+    }
+  }
+};
+</script>
+<style module src="./Menu.module.css"></style>
---
+++ src/pages/ThirdPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Third Page</h1>
+    <ThirdCard />
+  </div>
+</template>
+<script>
+import ThirdCard from '../components/ThirdCard.vue';
+export default {
+  components: { ThirdCard },
+};
+</script>
+<style scoped>
+/* Ваші стилі для ThirdPage */
+</style>
